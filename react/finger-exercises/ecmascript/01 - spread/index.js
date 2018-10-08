2175ca61b0e4b442f763e484c564b70930814938
Gabriel Porras <ghporras@gmail.com>
2018-10-08 09:54:20 -0500

    Doing "JS Stack II: Babel" - The test 3: "min can return the minimum based on many parameters" is   ready.

--- react/finger-exercises/ecmascript/01 - spread/index.js
+++ react/finger-exercises/ecmascript/01 - spread/index.js
@@ -5,7 +5,12 @@ export function min(numbers) {
     return Math.min(...numbers);
   }
 
-  return Math.min(numbers);
+  const myArgs = arguments;
+  if (myArgs.length === 1) {
+    return Math.min(myArgs[0]);
+  }
+
+  return Math.min(...myArgs);
 }
 
 export function copy() {
