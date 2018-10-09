738913d2150c3a4c2f140106fbf7823cc3d519a9
Gabriel Porras <ghporras@gmail.com>
2018-10-09 08:25:28 -0500

    Doing "JS Stack II: Babel" - The test 11: "delay works well" is ready.

--- react/finger-exercises/ecmascript/02 - promises/index.js
+++ react/finger-exercises/ecmascript/02 - promises/index.js
@@ -1,7 +1,11 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(delayTime) {
+  return new Promise(((resolve) => {
+    setTimeout(() => {
+      resolve(delayTime);
+    }, delay);
+  }));
 }
 
 export function asyncDelay() {
