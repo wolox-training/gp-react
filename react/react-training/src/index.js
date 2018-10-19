ddd9886a5bb7bd37c55ab2c2f489546d5029ddde
Gabriel Porras <ghporras@gmail.com>
2018-10-19 16:29:23 -0500

    Doing "ReactApp - Tic Tac Toe" - Ready the "Lifting State Up" step.   Now the board's state is the central mind and every time a square is   clicked it's updated.

--- react/react-training/src/index.js
+++ react/react-training/src/index.js
@@ -3,30 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
-
   render() {
     return (
       <button
         className="square"
         onClick={() => {
-          this.setState({ value: 'X' });
+          this.props.onClick({ value: 'X' });
         }}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-  static renderSquare(i) {
-    return <Square value={i} />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares });
+  }
+
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
 
   render() {
@@ -36,19 +42,19 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {Board.renderSquare(0)}
-          {Board.renderSquare(1)}
-          {Board.renderSquare(2)}
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {Board.renderSquare(3)}
-          {Board.renderSquare(4)}
-          {Board.renderSquare(5)}
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {Board.renderSquare(6)}
-          {Board.renderSquare(7)}
-          {Board.renderSquare(8)}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
       </div>
     );
