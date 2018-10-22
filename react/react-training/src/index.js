ca77a69a2e139b1bab21217df262cd8e6e7e6340
Gabriel Porras <ghporras@gmail.com>
2018-10-22 15:48:03 -0500

    Doing "ReactApp - Tic Tac Toe" - Ready the "Implementing Time Travel" step.   Now we can go thought the history of buttons clicked.

--- react/react-training/src/index.js
+++ react/react-training/src/index.js
@@ -48,32 +48,46 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      history: [{
+      history: [
+        {
           squares: Array(9).fill(null)
-        }],
+        }
+      ],
+      stepNumber: 0,
       xIsNext: true
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
+
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
-      history: history.concat([{
-        squares: squares,
-      }]),
+      history: history.concat([
+        {
+          squares: squares,
+        }
+      ]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
@@ -81,7 +95,7 @@ class Game extends React.Component {
           'Go to move #' + move :
           'Go to game start';
       return (
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
