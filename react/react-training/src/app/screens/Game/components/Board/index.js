df6f95653b01e13b6fc9ed27161e623104dc2bdd
Gabriel Porras <ghporras@gmail.com>
2018-10-24 18:39:13 -0500

    Doing "ReactApp - Tic Tac Toe" - Changed this <div> for a <fragment>.

--- react/react-training/src/app/screens/Game/components/Board/index.js
+++ react/react-training/src/app/screens/Game/components/Board/index.js
@@ -11,7 +11,7 @@ class Board extends Component {
 
   render() {
     return (
-      <div>
+      <fragment>
         <div className={styles.status}>{status}</div>
         <div className={styles.boardRow}>
           {this.renderSquare(0)}
@@ -28,7 +28,7 @@ class Board extends Component {
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
-      </div>
+      </fragment>
     );
   }
 }
