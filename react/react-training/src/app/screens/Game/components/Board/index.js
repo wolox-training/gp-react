e45b69edf9adcc49242c9999964ffb7671831763
Gabriel Porras <ghporras@gmail.com>
2018-10-29 09:09:54 -0500

    Doing "ReactApp - Tic Tac Toe" - Fixed the missing parameter of property arrayOf. - Fixed all ~ characters in Babel config file.

--- react/react-training/src/app/screens/Game/components/Board/index.js
+++ react/react-training/src/app/screens/Game/components/Board/index.js
@@ -41,7 +41,7 @@ class Board extends Component {
 
 Board.propTypes = {
   onClick: PropTypes.func.isRequired,
-  squares: PropTypes.arrayOf.isRequired
+  squares: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired
 };
 
 export default Board;
