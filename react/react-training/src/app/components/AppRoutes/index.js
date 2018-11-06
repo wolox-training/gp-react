f83f4cd7bc064894dd68acceda7028f8b7a55574
Gabriel Porras <ghporras@gmail.com>
2018-11-06 17:26:22 -0500

    Doing "ReactApp - Tic Tac Toe" - Now the routes are a constant.

--- react/react-training/src/app/components/AppRoutes/index.js
+++ react/react-training/src/app/components/AppRoutes/index.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Game from '@screens/Game';
 import Login from '@screens/Login';
+import { ROUTES } from '@constants/routes.js';
 
 export default class AppRoutes extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/game" component={Game} />
+          <Route exact path={ROUTES.HOME} component={Login} />
+          <Route exact path={ROUTES.GAME} component={Game} />
         </div>
       </Router>
     );
