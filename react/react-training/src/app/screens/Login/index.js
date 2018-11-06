656e7e6a898fb3bea2e580ab3a02c512123c5061
Gabriel Porras <ghporras@gmail.com>
2018-11-06 13:56:38 -0500

    Doing "ReactApp - Tic Tac Toe" - New method to do the redirect.

--- react/react-training/src/app/screens/Login/index.js
+++ react/react-training/src/app/screens/Login/index.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
 
 import styles from './styles.scss';
 
 class Login extends Component {
+  handleClick() {
+    window.location = '/game';
+  }
+
   render() {
     return (
       <div className={styles.form}>
         <h2 className={styles.title}>Login</h2>
         <input className={styles.input} type="text" placeholder="Username" name="username" />
         <input className={styles.input} type="password" placeholder="Password" name="password" />
-        <div>
-          <Route
-            render={({ history }) => (
-              <button className={styles.button} onClick={() => history.push('/game')}>
-                Sign in
-              </button>
-            )}
-          />
-        </div>
+        <button className={styles.button} onClick={this.handleClick}>
+          Sign in
+        </button>
       </div>
     );
   }
