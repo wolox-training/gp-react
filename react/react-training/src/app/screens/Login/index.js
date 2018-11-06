4ad38a5bc2c5baffc9e0d67b2cd2cadab2402924
Gabriel Porras <ghporras@gmail.com>
2018-11-06 14:41:39 -0500

    Doing "ReactApp - Tic Tac Toe" - Fixing 'Sign in' redirect.

--- react/react-training/src/app/screens/Login/index.js
+++ react/react-training/src/app/screens/Login/index.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from './styles.scss';
 
 class Login extends Component {
-  handleClick() {
-    window.location = '/game';
-  }
-
   render() {
     return (
       <div className={styles.form}>
         <h2 className={styles.title}>Login</h2>
         <input className={styles.input} type="text" placeholder="Username" name="username" />
         <input className={styles.input} type="password" placeholder="Password" name="password" />
-        <button className={styles.button} onClick={this.handleClick}>
-          Sign in
+        <button className={styles.button}>
+          <Link className={styles.link} to="/game">
+            Sign in
+          </Link>
         </button>
       </div>
     );
