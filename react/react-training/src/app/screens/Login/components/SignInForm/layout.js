f270456ad3dd01f76884fcca22be2a8598194ee0
Gabriel Porras <ghporras@gmail.com>
2018-11-15 11:05:43 -0500

    Doing "ReactApp - Tic Tac Toe" - The Login layout now is a Functional component

--- react/react-training/src/app/screens/Login/components/SignInForm/layout.js
+++ react/react-training/src/app/screens/Login/components/SignInForm/layout.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Field } from 'redux-form';
 import PropTypes from 'prop-types';
 
@@ -6,36 +6,34 @@ import { CustomField } from './components/CustomField';
 import { required, minLength, isEmail } from './validation';
 import styles from './styles.scss';
 
-class Layout extends Component {
-  render() {
-    const { handleSubmit, userLoginError } = this.props;
+function Layout(props) {
+  const { handleSubmit, userLoginError } = props;
 
-    return (
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <h2 className={styles.title}>Login</h2>
-        <Field
-          className={styles.input}
-          type="text"
-          placeholder="Username"
-          name="username"
-          component={CustomField}
-          validate={[required, isEmail]}
-        />
-        <Field
-          className={styles.input}
-          type="password"
-          placeholder="Password"
-          name="password"
-          component={CustomField}
-          validate={[required, minLength]}
-        />
-        <div>
-          <button className={styles.button}>Sign in</button>
-        </div>
-        {userLoginError && <div className={styles.error}>{userLoginError}</div>}
-      </form>
-    );
-  }
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <h2 className={styles.title}>Login</h2>
+      <Field
+        className={styles.input}
+        type="text"
+        placeholder="Username"
+        name="username"
+        component={CustomField}
+        validate={[required, isEmail]}
+      />
+      <Field
+        className={styles.input}
+        type="password"
+        placeholder="Password"
+        name="password"
+        component={CustomField}
+        validate={[required, minLength]}
+      />
+      <div>
+        <button className={styles.button}>Sign in</button>
+      </div>
+      {userLoginError && <div className={styles.error}>{userLoginError}</div>}
+    </form>
+  );
 }
 
 Layout.propTypes = {
