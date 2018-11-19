8ce6d53a7df45518a4fd75dd5a9b401a56f4e6ce
Gabriel Porras <ghporras@gmail.com>
2018-11-19 15:48:53 -0500

    Doing "ReactApp - Tic Tac Toe" - Removed from login service the unnecessaries response validations

--- react/react-training/src/redux/login/actions.js
+++ react/react-training/src/redux/login/actions.js
@@ -1,5 +1,5 @@
 import UserService from '@services/UserService.js';
-import { MSG_UNKNOWN_ID, ERROR_READING_RESPONSE, ERROR_LOGIN_SERVICE } from '@screens/Login/validation';
+import { MSG_UNKNOWN_ID, ERROR_READING_RESPONSE } from '@screens/Login/validation';
 
 export const actionTypes = {
   LOGIN: 'LOGIN',
@@ -11,38 +11,24 @@ export const actionsCreators = {
   login: (username, password) => async dispatch => {
     dispatch({ type: actionTypes.LOGIN });
     const response = await UserService.login(username, password);
-    if (response && response.data) {
-      if (response.ok) {
-        const userIsLogged = true;
-        const userLoginError = null;
-        const userSession = response.data.id || MSG_UNKNOWN_ID;
-        dispatch({
-          type: actionTypes.LOGIN_SUCCESS,
-          payload: {
-            userIsLogged,
-            userLoginError,
-            userSession
-          }
-        });
-      } else {
-        const userIsLogged = false;
-        const userLoginError =
-          response.data.error && response.data.error.message && response.data.error.statusCode
-            ? `Error ${response.data.error.statusCode} - ${response.data.error.message}`
-            : ERROR_READING_RESPONSE;
-        const userSession = null;
-        dispatch({
-          type: actionTypes.LOGIN_FAILURE,
-          payload: {
-            userIsLogged,
-            userLoginError,
-            userSession
-          }
-        });
-      }
+    if (response.ok) {
+      const userIsLogged = true;
+      const userLoginError = null;
+      const userSession = response.data.id || MSG_UNKNOWN_ID;
+      dispatch({
+        type: actionTypes.LOGIN_SUCCESS,
+        payload: {
+          userIsLogged,
+          userLoginError,
+          userSession
+        }
+      });
     } else {
       const userIsLogged = false;
-      const userLoginError = ERROR_LOGIN_SERVICE;
+      const userLoginError =
+        response.data && response.data.error && response.data.error.message && response.data.error.statusCode
+          ? `Error ${response.data.error.statusCode} - ${response.data.error.message}`
+          : ERROR_READING_RESPONSE;
       const userSession = null;
       dispatch({
         type: actionTypes.LOGIN_FAILURE,
