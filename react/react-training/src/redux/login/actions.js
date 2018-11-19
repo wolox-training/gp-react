9e6f1f54829b666b50d438ede81048e8771f760d
Gabriel Porras <ghporras@gmail.com>
2018-11-19 16:21:06 -0500

    Doing "ReactApp - Tic Tac Toe" - Fixed merge with the previous branch

--- react/react-training/src/redux/login/actions.js
+++ react/react-training/src/redux/login/actions.js
@@ -11,39 +11,20 @@ export const actionsCreators = {
   login: (username, password) => async dispatch => {
     dispatch({ type: actionTypes.LOGIN });
     const response = await UserService.login(username, password);
-    if (response && response.data) {
-      if (response.ok) {
-        const userIsLogged = true;
-        const userLoginError = null;
-        const userSession = response.data.id || MSG_UNKNOWN_ID;
-        localStorage.setItem('userIsLogged', true.toString());
-        localStorage.setItem('userSession', userSession);
-        dispatch({
-          type: actionTypes.LOGIN_SUCCESS,
-          payload: {
-            userIsLogged,
-            userLoginError,
-            userSession
-          }
-        });
-      } else {
-        const userIsLogged = false;
-        const userLoginError =
-          response.data.error && response.data.error.message && response.data.error.statusCode
-            ? `Error ${response.data.error.statusCode} - ${response.data.error.message}`
-            : ERROR_READING_RESPONSE;
-        const userSession = null;
-        localStorage.removeItem('userIsLogged');
-        localStorage.removeItem('userSession');
-        dispatch({
-          type: actionTypes.LOGIN_FAILURE,
-          payload: {
-            userIsLogged,
-            userLoginError,
-            userSession
-          }
-        });
-      }
+    if (response.ok) {
+      const userIsLogged = true;
+      const userLoginError = null;
+      const userSession = response.data.id || MSG_UNKNOWN_ID;
+      localStorage.setItem('userIsLogged', true.toString());
+      localStorage.setItem('userSession', userSession);
+      dispatch({
+        type: actionTypes.LOGIN_SUCCESS,
+        payload: {
+          userIsLogged,
+          userLoginError,
+          userSession
+        }
+      });
     } else {
       const userIsLogged = false;
       const userLoginError =
