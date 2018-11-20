1bda0de57a15f0d8abd804a93bb1753571220c2b
Gabriel Porras <ghporras@gmail.com>
2018-11-20 14:28:06 -0500

    Doing "ReactApp - Tic Tac Toe" - Changed Offline setting to false

--- react/react-training/src/app/screens/Dashboard/screens/Profile/index.js
+++ react/react-training/src/app/screens/Dashboard/screens/Profile/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
+import photo from './assets/ profilePic9.jpg';
 import styles from './styles.scss';
 
 function Profile() {
+  const nameUser = 'Johana Uribe';
   return (
-    <div>
-      <h2 className={styles.title}>Profile</h2>
-    </div>
+    <Fragment>
+      <div>
+        <h2 className={styles.title}>{nameUser}</h2>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.framework}>
+          <img
+            alt={nameUser}
+            className={styles.photo}
+            height={photo.height}
+            src={photo}
+            width={photo.width}
+          />
+        </div>
+        <div className={styles.info}>
+          <p className={styles.text}>
+            <span className={styles.user}>Gender:</span> Female
+            <br />
+            <span className={styles.user}>Region:</span> Colombia
+            <br />
+            <span className={styles.user}>Phone:</span>{' '}
+            <a href="tel:(514) 706 7652}" className={styles.link}>
+              (514) 706 7652
+            </a>
+            <br />
+            <span className={styles.user}>Birthday:</span> 04/09/1992
+            <br />
+            <span className={styles.user}>Email:</span>
+            &nbsp;
+            <a href="johanauribe@example.com" className={styles.link}>
+              johanauribe@example.com
+            </a>
+            <br />
+            <span className={styles.user}>Password:</span> Uribe92~
+          </p>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
