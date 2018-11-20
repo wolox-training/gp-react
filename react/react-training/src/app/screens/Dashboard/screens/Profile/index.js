7300decf4bc78429cc88fa9cc1858a0e42888034
Gabriel Porras <ghporras@gmail.com>
2018-11-20 16:37:14 -0500

    Doing "ReactApp - Tic Tac Toe": - Removed unnecessary div tag

--- react/react-training/src/app/screens/Dashboard/screens/Profile/index.js
+++ react/react-training/src/app/screens/Dashboard/screens/Profile/index.js
@@ -3,11 +3,7 @@ import React from 'react';
 import styles from './styles.scss';
 
 function Profile() {
-  return (
-    <div>
-      <h2 className={styles.title}>Profile</h2>
-    </div>
-  );
+  return <h2 className={styles.title}>Profile</h2>;
 }
 
 export default Profile;
