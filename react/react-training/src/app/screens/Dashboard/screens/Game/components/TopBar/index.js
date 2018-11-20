6f0bb4930aea9c1fdff9f70123db833cdb8efbbb
Gabriel Porras <ghporras@gmail.com>
2018-11-20 16:17:17 -0500

    Doing "ReactApp - Tic Tac Toe": - Fixed HTML of the Top Bar to a simpler one

--- react/react-training/src/app/screens/Dashboard/screens/Game/components/TopBar/index.js
+++ react/react-training/src/app/screens/Dashboard/screens/Game/components/TopBar/index.js
@@ -6,26 +6,24 @@ import styles from './styles.scss';
 function TopBar() {
   return (
     <nav className={styles.bar}>
-      <ul className={styles.list}>
-        <li className={styles.item}>
-          <i className="fas fa-gamepad" />
-          <NavLink activeClassName={styles.active} className={styles.link} to="/game/">
-            Game
-          </NavLink>
-        </li>
-        <li className={styles.item}>
-          <i className="fas fa-user" />
-          <NavLink activeClassName={styles.active} className={styles.link} to="/profile/">
-            Profile
-          </NavLink>
-        </li>
-        <li className={styles.item}>
-          <i className="fas fa-sign-out-alt" />
-          <NavLink activeClassName={styles.active} className={styles.link} to="/logout/">
-            Logout
-          </NavLink>
-        </li>
-      </ul>
+      <div className={styles.item}>
+        <i className="fas fa-gamepad" />
+        <NavLink activeClassName={styles.active} className={styles.link} to="/game/">
+          Game
+        </NavLink>
+      </div>
+      <div className={styles.item}>
+        <i className="fas fa-user" />
+        <NavLink activeClassName={styles.active} className={styles.link} to="/profile/">
+          Profile
+        </NavLink>
+      </div>
+      <div className={styles.item}>
+        <i className="fas fa-sign-out-alt" />
+        <NavLink activeClassName={styles.active} className={styles.link} to="/logout/">
+          Logout
+        </NavLink>
+      </div>
     </nav>
   );
 }
