b184755a01d9896a6fea2768893c0ef6546d3423
Gabriel Porras <ghporras@gmail.com>
2018-11-27 09:55:26 -0500

    Doing "ReactApp - Tic Tac Toe": - Using the Completer "CompleteReducer" and the Creator "CreateReducer"   from Redux Recompose in User Reducer

--- react/react-training/src/redux/user/actions.js
+++ react/react-training/src/redux/user/actions.js
@@ -1,6 +1,6 @@
 import UserService from '@services/UserService.js';
 import { completeTypes, createTypes } from 'redux-recompose';
-import { ERROR_READING_RESPONSE, MSG_UNKNOWN_USER } from '@screens/Login/validation';
+// import { ERROR_READING_RESPONSE, MSG_UNKNOWN_USER } from '@screens/Login/validation';
 
 const myActionTypes = ['USER_GET', 'USER_PATCH', 'USER_POST'];
 export const actionTypes = createTypes(completeTypes(myActionTypes));
@@ -13,61 +13,17 @@ export const actionsCreators = {
     payload: userId
   }),
 
-  userPatch: (userId, newUserData) => async dispatch => {
-    dispatch({ type: actionTypes.USER_PATCH });
-    let userData;
-    let userDataError;
-    const response = await UserService.userPatch(userId, newUserData);
-    if (response.ok) {
-      userData = response.data || MSG_UNKNOWN_USER;
-      userDataError = null;
-      dispatch({
-        type: actionTypes.USER_PATCH_SUCCESS,
-        payload: {
-          userData,
-          userDataError
-        }
-      });
-    } else {
-      userDataError =
-        response.data && response.data.error && response.data.error.message && response.data.error.statusCode
-          ? `Error ${response.data.error.statusCode} - ${response.data.error.message}`
-          : ERROR_READING_RESPONSE;
-      dispatch({
-        type: actionTypes.USER_PATCH_FAILURE,
-        payload: {
-          userDataError
-        }
-      });
-    }
-  },
+  userPatch: (userId, newUserData) => ({
+    type: actionTypes.USER_PATCH,
+    target: 'userData',
+    service: UserService.userPatch,
+    payload: { userId, newUserData }
+  }),
 
-  userPost: myUser => async dispatch => {
-    dispatch({ type: actionTypes.USER_POST });
-    let userData;
-    let userDataError;
-    const response = await UserService.userPost(myUser);
-    if (response.ok) {
-      userData = response.data || MSG_UNKNOWN_USER;
-      userDataError = null;
-      dispatch({
-        type: actionTypes.USER_POST_SUCCESS,
-        payload: {
-          userData,
-          userDataError
-        }
-      });
-    } else {
-      userDataError =
-        response.data && response.data.error && response.data.error.message && response.data.error.statusCode
-          ? `Error ${response.data.error.statusCode} - ${response.data.error.message}`
-          : ERROR_READING_RESPONSE;
-      dispatch({
-        type: actionTypes.USER_POST_FAILURE,
-        payload: {
-          userDataError
-        }
-      });
-    }
-  }
+  userPost: myUser => ({
+    type: actionTypes.USER_POST,
+    target: 'userData',
+    service: UserService.userPost,
+    payload: myUser
+  })
 };
--- react/react-training/src/services/UserService.js
+++ react/react-training/src/services/UserService.js
@@ -2,6 +2,6 @@ import api from '../config/api';
 
 export default {
   userGet: userId => api.get(`/Users/${userId}`),
-  userPatch: (userId, newUserData) => api.patch(`/Users/${userId}`, newUserData),
+  userPatch: ({ userId, newUserData }) => api.patch(`/Users/${userId}`, newUserData),
   userPost: user => api.post('/Users', user)
 };
