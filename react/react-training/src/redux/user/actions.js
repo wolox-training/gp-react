4e67622c81ac8aecb5415bdd579ca74887b63a72
Gabriel Porras <ghporras@gmail.com>
2018-11-28 07:07:07 -0500

    Doing "ReactApp - Tic Tac Toe": - Fixing minor details in User actions from Redux

--- react/react-training/src/redux/user/actions.js
+++ react/react-training/src/redux/user/actions.js
@@ -1,27 +1,28 @@
 import UserService from '@services/UserService.js';
 import { completeTypes, createTypes } from 'redux-recompose';
 
-const myActionTypes = ['USER_GET', 'USER_PATCH', 'USER_POST'];
-export const actionTypes = createTypes(completeTypes(myActionTypes));
+import { TARGE_USERDATA } from '@constants';
+
+export const actionTypes = createTypes(completeTypes(['USER_GET', 'USER_PATCH', 'USER_POST']));
 
 export const actionsCreators = {
   userGet: userId => ({
     type: actionTypes.USER_GET,
-    target: 'userData',
+    target: TARGE_USERDATA,
     service: UserService.userGet,
     payload: userId
   }),
 
   userPatch: (userId, newUserData) => ({
     type: actionTypes.USER_PATCH,
-    target: 'userData',
+    target: TARGE_USERDATA,
     service: UserService.userPatch,
     payload: { userId, newUserData }
   }),
 
   userPost: myUser => ({
     type: actionTypes.USER_POST,
-    target: 'userData',
+    target: TARGE_USERDATA,
     service: UserService.userPost,
     payload: myUser
   })
