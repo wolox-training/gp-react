d6e381b8ae64561cb87e632ea7416c702480ab31
Gabriel Porras <ghporras@gmail.com>
2018-11-30 15:51:11 -0500

    Doing "ReactApp - Tic Tac Toe": - Test "pow returns an array of power results if an array of pairs are   sent as arguments" ready

--- react/finger-exercises/testing_1/01 - matchers/test.js
+++ react/finger-exercises/testing_1/01 - matchers/test.js
@@ -10,7 +10,9 @@ describe('01 - matchers', () => {
   it('pow returns undefined if there is just one argument', () => {
     expect(pow(2)).toBeUndefined();
   });
-  xit('pow returns an array of power results if array of pairs are sent as arguments', () => {
+  it('pow returns an array of power results if an array of pairs are sent as arguments', () => {
+    expect(pow([2, 2], [3, 3])).toContain(4);
+    expect(pow([2, 2], [3, 3])).toContain(27);
   });
   xit('pow returns undefined in the right position of the result array if pair is not as expected', () => {
   });
