dfa8a078cf144db70d1073e0f16fdf99e806a32e
Gabriel Porras <ghporras@gmail.com>
2018-11-30 16:11:58 -0500

    Doing "ReactApp - Tic Tac Toe": - Test "getData resolves the data if true is sent as an argument (use   async/await)" ready

--- react/finger-exercises/testing_1/02 - async/test.js
+++ react/finger-exercises/testing_1/02 - async/test.js
@@ -1,8 +1,9 @@
 import getData from '.';
 
 describe('02 - async', () => {
-  xit('getData resolves the data if true is sent as argument (use async/await)', async () => {
-    getData();
+  it('getData resolves the data if true is sent as an argument (use async/await)', async () => {
+    expect.assertions(1);
+    await expect(getData(true)).resolves.toBe('data');
   });
   xit('getData resolves the data if true is sent as argument (avoid async/await)', () => {
   });
