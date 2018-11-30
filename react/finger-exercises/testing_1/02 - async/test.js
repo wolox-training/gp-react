3a02ad15f4d427f1dc08961b19ea34537b633467
Gabriel Porras <ghporras@gmail.com>
2018-11-30 16:32:09 -0500

    Doing "ReactApp - Tic Tac Toe": - Test "getData throws error if false is sent as argument (use   async/await)" ready

--- react/finger-exercises/testing_1/02 - async/test.js
+++ react/finger-exercises/testing_1/02 - async/test.js
@@ -9,7 +9,9 @@ describe('02 - async', () => {
     expect.assertions(1);
     return expect(getData(true)).resolves.toBe('data');
   });
-  xit('getData throws error if false is sent as argument (use async/await)', async () => {
+  it('getData throws error if false is sent as argument (use async/await)', async () => {
+    expect.assertions(1);
+    await expect(getData(false)).rejects.toThrow();
   });
   xit('getData throws error if false is sent as argument (avoid async/await)', () => {
   });
